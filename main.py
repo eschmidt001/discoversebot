69f2e37e80c598bd3619a59097c07c7ec362edf1
Elisa Schmidt <es797@cornell.edu>
2022-01-27 20:26:34 -0500

    lyrics but not really

---
+++ main.py
@@ -0,0 +1,24 @@
+#fuck i haven't touched python in months
+#all thaaaaaaaaaat jazz....BORING but required
+import numpy as np 
+#import tweepy
+import json
+
+#TODO: credential stuff from separate file ONCE THEY GIVE THEM TO ME
+#lord please let me always use the right python interpreter
+
+#here's the plan:
+#take lyrics 
+f = open('Lyrics_fredodisco.json')
+#returns JSON object as a dictionary
+g = json.load(f)['songs']
+
+print(type(g))  
+#select spot to start
+
+
+#choose length
+
+#tweet out length
+
+#yeehaw that's the program
