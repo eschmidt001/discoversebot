8b420a10fcdee005e1975e2a4780edab03c345ff
Elisa Schmidt <es797@cornell.edu>
2022-01-27 20:36:41 -0500

    lyrics fix

--- .gitignore/lyrics.py
+++ .gitignore/lyrics.py
@@ -10,5 +10,6 @@ gtoken = 'X781DaTOSyVR_DR2EgevfwPEsWqoSyw5cLwmUmKN4ced4KbpVst6GoRIQLV2jiaa'
 
 genius = Genius(gtoken)
 artist = genius.search_artist("fredo disco", max_songs=4, sort="title")
+artist = genius.search_artist("fredo disco", sort="title")
 print(artist.songs)
 artist.save_lyrics()
