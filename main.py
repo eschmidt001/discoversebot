aae949b2cb841d77ecf3bd78f598887c0e7179b4
Elisa Schmidt <es797@cornell.edu>
2022-01-29 14:26:56 -0500

    text preprocessing, tweet length extraction

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 #i haven't touched python in months
 #all thaaaaaaaaaat jazz....BORING but required
+#from types import NoneType
 import numpy as np 
 #import tweepy
 import json
 import random
+import re
 
 #TODO: credential stuff from separate file ONCE THEY GIVE THEM TO ME
 #lord please let me always use the right python interpreter
 
-#here's the plan:
-#take lyrics 
+#here's the plan: take lyrics 
 f = open('Lyrics_fredodisco.json')
 g = json.load(f) #dictionary
-
 songs = g['songs'] #list of dictionaries/song
 
 #select song and spot to start
-n1 = random.randint(0, len(songs))
+n1 = random.randint(0, len(songs)-1)
 lyr = songs[n1]['lyrics']
-lyr = lyr[:lyr.find('1')]
-#remove '1embed'.  can also search for it in last line and remove more elegantly, since sometimes last letter cut off
 
+#preprocessing
+x = re.search(r'[0-9]+[E,e]', lyr) #remove 'embed' and surrounding numbers. 
+if x is None:
+    x = re.search(r'[E,e]mbed', lyr)
+lyr = lyr[:x.start()]
+lyr = lyr.replace('\u2005', ' ') #remove fake spaces
+lyr = lyr.replace('\\', "") #remove backslashes
 #this likely be done more elegantly using filter and regular expressions
 lines = lyr.splitlines()[1:] #cut out title
 while "" in lines:
@@ -29,12 +34,20 @@ for l in lines:
     if l[0] == '[': #remove words in brackets
         lines.remove(l)
 
-#also need to find and replace instances of \u2005 with '' within strings
-#I wonder if that's something genius uses to rat out who's using their lyrics
-print(lines)
-n2 = random.randint(0,len(lines)-1)
-#choose length (tweet limit is 280 chars)
-
+print(songs[n1]['title'])
+n2 = random.randint(0,len(lines)-3) #choose length (tweet limit is 280 chars)
+t = lines[n2]
+i = 1
+while len(t) < 280:
+    if (n2+i) < (len(lines)-1):
+        t = t + lines[n2+i]
+        i = i+1
+    else: break
+if len(t) > 279:
+    tw = lines[n2:n2+i-2]
+else: tw = lines[n2:n2+i]
+print(len(''.join(tw)))
+print(tw)
 #tweet out length
 
 #yeehaw that's the program
